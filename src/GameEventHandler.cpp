894a6bbb5dfdd0aa1e544f070cca94ed57e60bdd
naitro2010 <108594252+naitro2010@users.noreply.github.com>
2025-02-18 13:52:07 -0800

    untested backported version with one additional fix for SHAPEDATA

--- src/GameEventHandler.cpp
+++ src/GameEventHandler.cpp
@@ -77,18 +77,49 @@ namespace plugin {
 			uint32_t expected = 0;
 			if (skee_loaded.compare_exchange_strong(expected, 1) == true && expected == 0) {
 				logger::info("Got SKEE64 information");
-				uint8_t signature[] = { 0xff, 0x90, 0xf0, 0x03, 0x00, 0x00 };
-				if (memcmp(signature, (void*)((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0xc2950 + (uintptr_t)0x28), sizeof(signature)) == 0) {
+				uint8_t signature1170[] = { 0xff, 0x90, 0xf0, 0x03, 0x00, 0x00 };
+				if (memcmp(signature1170, (void*)((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0xc2950 + (uintptr_t)0x28), sizeof(signature)) == 0) {
 					uintptr_t patch0=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x1cea8);
                     uintptr_t patch1=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x1cebd);
                     uintptr_t patch2=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x1cec8);
+                    uintptr_t patch3=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x1bd58);
+                    uintptr_t patch4=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x1bd6a);
 		            REL::safe_write(patch0,(uint8_t*)"\x8b\xca\x90\x90",4);
                     REL::safe_write(patch1,(uint8_t*)"\x90\x90\x90\x90\x90\x90\x90\x90",8);
                     REL::safe_write(patch2,(uint8_t*)"\x90\x90\x90\x90\x90\x90\x90\x90",8);
+                    REL::safe_write(patch3,(uint8_t*)"\x8b\xd1\x90\x90",4);
+                    REL::safe_write(patch4,(uint8_t*)"\x90\x90",4);
 					logger::info("SKEE64 patched");
-				} else {
-					logger::error("Wrong SKEE64 version");
 				}
+                else if ((skee64_info.SizeOfImage >= 0x16b478+7) && memcmp("BODYTRI",(void*)((uintptr_t)skee64_info.lpBaseOfDll+(uintptr_t)0x16b478),7)) {
+                    uintptr_t patch0=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x18438);
+                    uintptr_t patch1=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x1844d);
+                    uintptr_t patch2=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x18458);
+                    uintptr_t patch3=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x7798);
+                    uintptr_t patch4=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0x77aa);
+                    REL::safe_write(patch0,(uint8_t*)"\x8b\xca\x90\x90",4);
+                    REL::safe_write(patch1,(uint8_t*)"\x90\x90\x90\x90\x90\x90\x90\x90",8);
+                    REL::safe_write(patch2,(uint8_t*)"\x90\x90\x90\x90\x90\x90\x90\x90",8);
+                    REL::safe_write(patch3,(uint8_t*)"\x8b\xd1\x90\x90",4);
+                    REL::safe_write(patch4,(uint8_t*)"\x90\x90",4);
+                    logger::info("SKEE64 0416 patched");
+                }
+                else if ((skee64_info.SizeOfImage >= 0x17ec68+7) && memcmp("BODYTRI",(void*)((uintptr_t)skee64_info.lpBaseOfDll+(uintptr_t)0x17ec68),7)) {
+                    uintptr_t patch0=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0xbfd8);
+                    uintptr_t patch1=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0xbfed);
+                    uintptr_t patch2=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0xbff8);
+                    uintptr_t patch3=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0xafd8);
+                    uintptr_t patch4=((uintptr_t)skee64_info.lpBaseOfDll + (uintptr_t)0xafea);
+                    REL::safe_write(patch0,(uint8_t*)"\x8b\xca\x90\x90",4);
+                    REL::safe_write(patch1,(uint8_t*)"\x90\x90\x90\x90\x90\x90\x90\x90",8);
+                    REL::safe_write(patch2,(uint8_t*)"\x90\x90\x90\x90\x90\x90\x90\x90",8);
+                    REL::safe_write(patch3,(uint8_t*)"\x8b\xd1\x90\x90",4);
+                    REL::safe_write(patch4,(uint8_t*)"\x90\x90",4);
+                    logger::info("SKEE64 04194 patched");
+                } else {
+                    logger::error("Wrong SKEE64 version");
+                }
+				
 			}
 		} else {
 			logger::error("Get SKEE64 last error {}", GetLastError());
